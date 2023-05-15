af762e67cf7aaebb2d3ebd6f04a08977bb958e3b
Aleksandr Bondarev <alexandr.s.bondarev@yandex.ru>
2023-05-15 21:49:36 +0600

    add Parametrized tests for BunTest, fixed pom maven version

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,24 +1,42 @@
 package praktikum;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class BunTest {
     Bun bun;
+    private String bunName;
+    private float price;
 
-    @Before
-    public void setUp(){
-        bun = new Bun("Флюоресцентная булка R2-D3", 988f);
+    public BunTest(String bunName, float price){
+        this.bunName = bunName;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] bunTest() {
+        return new Object[][]{
+                {"Флюоресцентная булка R2-D3", 988f},
+                {"", -1f},
+                {null, 0f},
+                {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0.01f},
+                {"!“№ ;%: ?*()_+/, § $&= @#«» <>~®-;²³", 99999999999999999999999f},
+                {"1337", -0f},
+        };
     }
 
     @Test
     public void getName() {
-        Assert.assertEquals("Флюоресцентная булка R2-D3", bun.getName());
+        bun = new Bun(bunName,988f);
+        Assert.assertEquals(bunName, bun.getName());
     }
 
     @Test
     public void getPrice() {
-        Assert.assertEquals(988f, bun.getPrice(),0);
+        bun = new Bun("Флюоресцентная булка R2-D3",price);
+        Assert.assertEquals(price, bun.getPrice(),0);
     }
 }
